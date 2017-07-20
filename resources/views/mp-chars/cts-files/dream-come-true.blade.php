558311e3c4b12ba94dfcb05d66b35ccc525e6f7e
CtreuseLex <ctreuse109@gmail.com>
2017-07-20 23:56:37 +0800

    july update 5

--- resources/views/mp-chars/cts-files/dream-come-true.blade.php
+++ resources/views/mp-chars/cts-files/dream-come-true.blade.php
@@ -9,14 +9,14 @@
         Text Message by <span class="cts-mikael"></span> to <span class="cts-maximus"></span>
         <br>TIME: 1697, SR 4th Fall
         <br>
-        <br>Hey Max, thanks again for the help on my CyberTech Suit. Are you sure you don't want me give you any kind of credit for this. It seems like you went overboard with the details and functions of this suit. It's more, more, MORE than what I designed it to be. Our week has been kind of a burnout for me, and I believe it does the same for you too. So are you sure you don't want you name on this? Just reminding you that this suit turned out to be very similar to what you want to go with the one you... you're trapped in. So upgrading that suit can get you backslash from the media. I can help with that yes, but that would end up being a late save if you don't have any kind of contract with me on the making of this suit. Plus I'm going to get a lot of credits for this suit, you know. Just saying. Are you sure you don't want your name in?
+        <br>Hey Max, thanks again for the help on my CyberTech Suit. Are you sure you don't want me give you any kind of credit for this. It seems like you went overboard with the details and functions of this suit. It's more, more, MORE than what I designed it to be. Our week has been kind of a burnout for me, and I believe it does the same for you too. So are you sure you don't want your name on this? Just reminding you that this suit turned out to be very similar to what you want to go with the one you... you're trapped in. So upgrading that suit can get you backslash from the media. I can help with that yes, but that would end up being a late save if you don't have any kind of contract with me on the making of this suit. Plus I'm going to get a lot of credits for this suit, you know. Just saying. Are you sure you don't want your name in?
 
         <hr>
         Text Message by <span class="cts-maximus"></span> to <span class="cts-mikael"></span>
         <br>TIME: 1697, SR 5th Fall
         <br>
-        <br>Yes Mike. You're welcome
-        <br>I can go ahead and wreck havoc with that suit. I'm not replacing or upgrading mine. Because of reasons. Thanks.
+        <br>You really need to lower your sentence count and yes Mike. You're welcome.
+        <br>You can go ahead and wreck havoc with that suit. I'm not replacing or upgrading mine. Because of reasons. Thanks.
         <br>I'm actually stopping the whole project now. It's not that it's been idle for half a decade now.
         <hr>
 
